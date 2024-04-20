04ce2c78eda6ddbfa34bcb6ca1a7c1e96c466f6f
Umaya <it21306136@my.sliit.lk>
2024-04-20 21:13:35 +0530

    CreateWorkout Page

--- frontend/src/pages/createWorkout/CreateWorkout.jsx
+++ frontend/src/pages/createWorkout/CreateWorkout.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import './CreateWorkout.css'
 
 function CreateWorkout() {
   return (
-    <div>CreateWorkout</div>
+    <div className='CreateWorkout'>
+      <h1>Create New Workout</h1>
+      <form className='form-section'>
+        <div className='input-section'>
+          <label>Workout </label>
+          <input type='text' placeholder='Workout Name' required/>
+        </div>
+        <div className='img-upload'>
+        
+          <div className='file-section'>
+          
+          <input type='file' required/>
+          </div>
+          <div className='uploadBtn'>
+            <button type='button'>UPLOAD </button>
+          </div>
+        </div>
+
+        <div className='submitBtn'>
+          <button type='submit'>Share Workout</button>
+        </div>
+
+      </form>
+    </div>
   )
 }
 
