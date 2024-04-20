66bd59a21182b929fe85e6c746c70cabfb1df3c2
Umaya <it21306136@my.sliit.lk>
2024-04-20 22:19:58 +0530

    Create Updateworkout page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import CreateWorkout from "./pages/createWorkout/CreateWorkout";
+import Updateworkout from "./pages/updateWorkout/Updateworkout";
 import Home from "./pages/home/Home";
 import {Routes,Route} from "react-router-dom";
 
@@ -9,6 +10,7 @@ function App() {
      <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/createWorkout" element={<CreateWorkout/>}/>
+      <Route path="/updateWorkout" element={<Updateworkout/>}/>
       </Routes> 
     </div>
   );
---
+++ frontend/src/pages/updateWorkout/Updateworkout.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Updateworkout.css'
+import workoutimage from '../../images/workoutimage.jpeg'
+
+function Updateworkout() {
+  return (
+    <div className='Updateworkout'>
+        <h1>Edit Workout</h1>
+      <form className='form-section'>
+        <div className='input-section'>
+          <label>Workout </label>
+          <input type='text' placeholder='Workout Name' required/>
+        </div>
+        <div className='img-upload'>
+        
+          <div className='file-section'>
+          
+          <input type='file' required/>
+          </div>
+          <div className='uploadBtn'>
+            <button type='button'>UPLOAD </button>
+          </div>
+        </div>
+
+        <div className='AddDelete'>
+            <img src={workoutimage}/>
+            <button type='button'>Delete</button>
+        </div>
+
+        <div className='submitBtn'>
+          <button type='submit'>Update Workout</button>
+
+        </div>
+
+      </form>
+    </div>
+  )
+}
+
+export default Updateworkout
