933efd33fa8f90833f61283a420020b3ac6a1ad1
200011801179 <it21216732@my.sliit.lk>
2024-04-21 16:21:56 +0530

    src_F

---
+++ BackEnd/src/main/java/com/food_app/foodapp/model/WorkOutPlan/WorkOutPlan.java
@@ -0,0 +1,45 @@
+package com.food_app.foodapp.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "workload_plans")
+public class WorkOutPlan {
+    @Id
+    private String id;
+    private String name;
+    private String routine;
+    private String[] notes;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRoutine() {
+        return routine;
+    }
+
+    public void setRoutine(String routine) {
+        this.routine = routine;
+    }
+
+    public String[] getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String[] notes) {
+        this.notes = notes;
+    }
+}
