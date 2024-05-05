268030d3700e0f001a55b3f5702d388e439a7386
Umaya <it21306136@my.sliit.lk>
2024-05-05 22:08:04 +0530

    Add Date field

--- frontend/src/pages/AddWorkout/AddWorkout.jsx
+++ frontend/src/pages/AddWorkout/AddWorkout.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
 import './AddWorkout.css'
 
-function CreateMeal() {
+function AddWorkout() {
   return (
-    <div className='CreateMeal'>
+    <div className='AddWorkout'>
         <h1>Add Workout Plan</h1>
         <form className='form-section'>
         <div className='input-section'>
+          <label>Title </label>
+          <input type='text' placeholder='Title' required/>
           <label>Name </label>
-          <input type='text' placeholder='Meal Name' required/>
-          <label>Description </label>
-          <input type='text' placeholder='Description' required/>
-          <label>Category </label>
-          <input type='text' placeholder='category' required/>
+          <input type='text' placeholder='Name' required/>
+          <label>Created Date </label>
+          <input type='date' placeholder='Date' required/>
+          <label>Age </label>
+          <input type='text' placeholder='age' required/>
         </div>
-        <div className='img-upload'></div>
+        
         
           
 
@@ -23,4 +25,4 @@ function CreateMeal() {
   )
 }
 
-export default CreateMeal
+export default AddWorkout
