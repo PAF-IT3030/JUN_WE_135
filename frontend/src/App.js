73aa83636e4994eb1bf4d898a5f41ebf4f3a72b4
Umaya <it21306136@my.sliit.lk>
2024-05-06 22:22:49 +0530

    path for EditWorkoutPlan

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import CreateWorkout from "./pages/createWorkout/CreateWorkout";
 import Updateworkout from "./pages/updateWorkout/Updateworkout";
 import AddWorkout from "./pages/AddWorkout/AddWorkout";
+import EditWorkoutPlan from "./pages/EditWorkoutPlan/EditWorkoutPlan";
 import Home from "./pages/home/Home";
 import {Routes,Route} from "react-router-dom";
 
@@ -13,6 +14,7 @@ function App() {
       <Route path="/createWorkout" element={<CreateWorkout/>}/>
       <Route path="/updateWorkout" element={<Updateworkout/>}/>
       <Route path="/AddWorkout" element={<AddWorkout/>}/>
+      <Route path="/EditWorkoutPlan" element={<EditWorkoutPlan/>}/>
       </Routes> 
     </div>
   );
